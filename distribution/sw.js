03849266f5cf614ef4c4e60a592edabcde4cfed4
Евгений Баян <THEklasterstorm@gmail.com>
2025-11-24 01:11:13 +0300

    Test

--- distribution/sw.js
+++ distribution/sw.js
@@ -1,10 +1,10 @@
 self.addEventListener('install', (event) => {
-    self.skipWaiting();
     console.log('install event');
     console.log(event);
+    self.skipWaiting();
 });
 self.addEventListener('activate', (event) => {
-  event.waitUntil(clients.claim());
     console.log('activate event');
     console.log(event);
+    event.waitUntil(clients.claim());
 });
